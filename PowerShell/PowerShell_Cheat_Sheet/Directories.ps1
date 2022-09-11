6c9dbe452870f852b6aa7e8b33b1c5d35e52a4d2
BiGSteve-0 <steven.d.harris2@gmail.com>
2022-09-11 18:44:34 -0400

    First Commit of Helpful Terms and Syntax

---
+++ PowerShell/PowerShell_Cheat_Sheet/Directories.ps1
@@ -0,0 +1,12 @@
+<#       ------------        PowerShellCommands      -------------            #>
+
+##drop down to root folder
+cd\
+##drop back one folder
+cd..
+##change drive
+C:
+## moving down the directory 
+cd \child item
+## list directory
+Ls or dir or Get-ChildItem
