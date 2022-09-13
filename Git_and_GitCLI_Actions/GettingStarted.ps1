d6e17b9493a602299b90e50573d911303bfd346e
BiGSteve-0 <steven.d.harris2@gmail.com>
2022-09-13 19:51:07 -0400

    work plz

---
+++ Git_and_GitCLI_Actions/GettingStarted.ps1
@@ -0,0 +1,14 @@
+<#  ---------- GitCLI ----------- #>
+
+## Winget to insall GitHub CLI
+winget install wingetcreate
+
+## GitHub CLI used for GitHub commands
+winget install --id GitHub.CLI
+
+## Login Git
+gh auth Login
+
+##
+
+
